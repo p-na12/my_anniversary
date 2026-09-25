130ed728a514e707f502a1ded6ed19fd06b52b3a
lina <polinapang80@gmail.com>
2026-09-25 17:06:00 +0700

    fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/my_anniversary/",
+  base: process.env.VERCEL ? "/" : "/my_anniversary/",
 });
